c0a16d5adbf1439ca35309ac7560b70f73000e00
Angelo Thys <thys.angelo@gmail.com>
2024-03-30 19:05:44 +0000

    Add LazyExtras

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.completeopt = "menu,menuone,preview,noinsert,noselect"
+opt.scrolloff = 8
+opt.wrap = true
+opt.colorcolumn = "80"
